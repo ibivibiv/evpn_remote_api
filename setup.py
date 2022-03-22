1920f002ec0eea4e8ab4c8f7bc8a25e3fbe6af72
Harold Hannon <haroldhannon@gmail.com>
2022-03-22 10:33:56 -0500

    Add files via upload
    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='evpn_remote_api',
+    version='0.1',
+    packages=['evpn_remote_api'],
+    url='',
+    license='none',
+    author='haroldhannon',
+    author_email='hhannon@us.ibm.com',
+    description='quick evpn client for setting up bridges, vxlan interfaces, frr, and tap interfaces'
+)
